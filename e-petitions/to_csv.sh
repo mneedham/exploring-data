d051035508037e7428fa37404c78c0963b34f38c
Mark Needham <mark.needham@neotechnology.com>
2016-04-06 06:34:08 +0100

    putting into scripts folder

--- e-petitions/to_csv.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-for file in `find data -iname *.json`;  do
-  # jq -r ".items[] | [.identifier._value , .sponsorPrinted[], .created._value, .label._value, .numberOfSignatures, .status] | @csv" ${file}
-  jq -r ".items[] | [.identifier._value] | @csv" ${file}
-done
