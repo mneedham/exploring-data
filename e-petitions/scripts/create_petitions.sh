df87a81c1d27bbaef4efd36aee0c8e86d01315c2
Mark Needham <mark.needham@neotechnology.com>
2016-04-07 06:29:03 +0100

    adding link to CSV file

--- e-petitions/scripts/create_petitions.sh
+++ e-petitions/scripts/create_petitions.sh
@@ -1,24 +1,25 @@
 #!/bin/bash
 
-echo "action,background,additionalDetails,state,signatureCount,createdAt,updatedAt,openAt,closedAt,governmentResponseAt,scheduledDebateDate,debateThresholdReachedAt,rejectedAt,debateOutcomeAt,moderationThresholdReachedAt,responseThresholdReachedAt,creatorName" > data/all_the_petitions.csv
+echo "link,action,background,additionalDetails,state,signatureCount,createdAt,updatedAt,openAt,closedAt,governmentResponseAt,scheduledDebateDate,debateThresholdReachedAt,rejectedAt,debateOutcomeAt,moderationThresholdReachedAt,responseThresholdReachedAt,creatorName" > data/all_the_petitions.csv
 
 for file in `find data/petitions -iname *.json`; do
-  jq -r ".data.attributes |
-  [.action,
-   .background,
-   .additional_details,
-   .state,
-   .signature_count,
-   .created_at,
-   .updated_at,
-   .open_at,
-   .closed_at,
-   .government_response_at,
-   .scheduled_debate_date,
-   .debate_threshold_reached_at,
-   .rejected_at,
-   .debate_outcome_at,
-   .moderation_threshold_reached_at,
-   .response_threshold_reached_at,
-   .creator_name] | @csv" ${file}
+  jq -r "{attributes: .data.attributes, link: .links.self} |
+  [.link,
+   .attributes.action,
+   .attributes.background,
+   .attributes.additional_details,
+   .attributes.state,
+   .attributes.signature_count,
+   .attributes.created_at,
+   .attributes.updated_at,
+   .attributes.open_at,
+   .attributes.closed_at,
+   .attributes.government_response_at,
+   .attributes.scheduled_debate_date,
+   .attributes.debate_threshold_reached_at,
+   .attributes.rejected_at,
+   .attributes.debate_outcome_at,
+   .attributes.moderation_threshold_reached_at,
+   .attributes.response_threshold_reached_at,
+   .attributes.creator_name] | @csv" ${file}
 done >> data/all_the_petitions.csv
